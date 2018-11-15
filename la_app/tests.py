31789ee8e8d994106244f3652a3e555cdce590df
kziete <sdiaz@talana.com>
2018-11-15 00:17:17 -0300

    api test

--- la_app/tests.py
+++ la_app/tests.py
@@ -2,6 +2,8 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
+from rest_framework.test import APITestCase
+
 from la_app.models import ModeloUno
 # Create your tests here.
 
@@ -40,4 +42,43 @@ class WebTest(TestCase):
         resp = self.client.post("/agregar", data)
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(len(resp.context["form"].errors), 1)
+
+class ApiTest(APITestCase):
+    def setUp(self):
+        modelos = [
+            ModeloUno(nombre="Primero"),
+            ModeloUno(nombre="Segundo"),
+        ]
+        ModeloUno.objects.bulk_create(modelos)
+
+    def test_lista(self):
+        resp = self.client.get("/api/modelo-uno/")
+
+        data = resp.json()
+
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(len(data), 2)
+    
+    def test_detalle(self):
+        m1 = ModeloUno.objects.all()[0]
+        resp = self.client.get("/api/modelo-uno/%d/" % m1.id)
+
+        data = resp.json()
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(data["nombre"], m1.nombre)
+        
+    def test_crear_ok(self):
+        data = {
+            'nombre': 'holo'
+        }
+        resp = self.client.post("/api/modelo-uno/", data)
+        self.assertEqual(resp.status_code, 201)
+    
+    def test_crear_fail(self):
+        data = {}
+        resp = self.client.post("/api/modelo-uno/", data)
+
+        data = resp.json()
+        self.assertEqual(resp.status_code, 400)
+        self.assertTrue("nombre" in data)
 
